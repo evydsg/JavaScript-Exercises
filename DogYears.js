6db7ceb10ec40c06f9c1d9960e51b16962233637
Evelise Karen Guenda <118639499+evydsg@users.noreply.github.com>
2023-09-07 17:05:04 -0500

    DogYears

---
+++ DogYears.js
@@ -0,0 +1,21 @@
+//My current age
+var myAge = 23;
+
+let earlyYears = 2;
+earlyYears *=10.5;
+
+myAge -=2;
+let laterYears = myAge;
+laterYears *=4;
+
+console.log(earlyYears);
+
+console.log(laterYears);
+
+//Calculating the age of my dog
+myAgeInDogYears = earlyYears + laterYears;
+
+let myName = "Evy";
+myName.toLowerCase();
+console.log(myName.toLowerCase());
+console.log(`My name is ${myName}. I am ${myAge} in human years which is ${myAgeInDogYears} years old in dog years.`);
